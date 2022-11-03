c83b25417007b8db07e2700bcaa9874b4f64224e
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2022-11-03 00:45:30 -0700

    Compilation on Perlmutter is working, changed to cblas_dsymv and cblas_dsyrk.

--- cpp_ext/test.cpp
+++ cpp_ext/test.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
-#include "oneapi/mkl.hpp"
 
 using namespace std;
 
 int main(int argc, char** argv) {
     cout << "Hello world!" << endl;
-    double* x = (double*) malloc(5 * sizeof(double)); 
-    double* y = (double*) malloc(5 * sizeof(double)); 
-    double res = cblas_ddot(5, x, 1, y, 1);
-    cout << res << endl;
 }
