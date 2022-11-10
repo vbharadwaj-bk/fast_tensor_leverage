62ead7c10c7b5a0eeac139754cb479f131866c4f
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2022-11-10 00:25:34 -0800

    True solution and approximate downsampled solution agree... now need to check why analytic calculation is failing.

--- cpp_ext/efficient_krp_sampler.cpp
+++ cpp_ext/efficient_krp_sampler.cpp
@@ -11,6 +11,7 @@
 using namespace std;
 
 class __attribute__((visibility("hidden"))) EfficientKRPSampler {
+public:
     uint64_t N, J, R, R2;
     vector<Buffer<double>> &U;
     ScratchBuffer scratch;
@@ -30,7 +31,6 @@ class __attribute__((visibility("hidden"))) EfficientKRPSampler {
     std::mt19937 gen;
     std::uniform_real_distribution<> dis;
 
-public:
     EfficientKRPSampler(
             uint64_t J, 
             uint64_t R, 
@@ -273,13 +273,34 @@ public:
         sampler.KRPDrawSamples(j, samples, &random_draws); 
     }
 
-    void KRPDrawSamples(uint32_t j, py::array_t<uint64_t> samples_py) {
+    void KRPDrawSamples(uint32_t j, 
+            py::array_t<uint64_t> samples_py, 
+            py::array_t<double> h_out_py) {
         Buffer<uint64_t> samples(samples_py);
-        sampler.KRPDrawSamples(j, samples, nullptr); 
+        Buffer<double> h_out(h_out_py);
+        sampler.KRPDrawSamples(j, samples, nullptr);
+        std::copy(sampler.h(), sampler.h(sampler.J * sampler.R), h_out()); 
+    }
+
+    void get_G_pinv(py::array_t<double> G_py) {
+        Buffer<double> G(G_py);
+        std::copy(sampler.M(), sampler.M(sampler.R2), G());
+    }
+};
+
+class __attribute__((visibility("hidden"))) CP_Decomposition {
+public:
+    unique_ptr<NPBufferList<double>> U_py_bufs;
+    int64_t R;
+    CP_Decomposition(int64_t R, py::list U_py)
+    :
+    U_py_bufs(new NPBufferList<double>(U_py))
+    {
+        this->R = R;
     }
 
     // Convenience method for RHS sampling 
-    void materialize_downsampled_rhs(py::array_t<uint64_t> samples_py, 
+    void materialize_partial_evaluation(py::array_t<uint64_t> samples_py, 
         uint64_t j,
         py::array_t<double> result_py 
         ) {
@@ -288,21 +309,21 @@ public:
         Buffer<uint64_t> samples(samples_py);
         uint64_t num_samples = samples.shape[1];
         Buffer<double> result(result_py);
-        std::fill(result(), result(J, 0), 1.0);
+        std::fill(result(), result(num_samples, 0), 1.0);
 
-        #pragma omp parallel for
-        for(int i = 0; i < num_samples; i++) {
-            for(int k = 0; k < N; k++) {
+        uint32_t N = U_py_bufs->length;
+
+        #pragma omp parallel for 
+        for(uint32_t i = 0; i < num_samples; i++) {
+            for(uint32_t k = 0; k < N; k++) {
                 if(k != j) {
                     for(uint32_t u = 0; u < R; u++) {
-                        result[i * R + u] *= U[samples[k * num_samples + i] * R + u];
+                        result[i * R + u] *= U_py_bufs->buffers[k][samples[k * num_samples + i] * R + u];
                     }
                 } 
             }
         }
-
     }
-
 };
 
 PYBIND11_MODULE(efficient_krp_sampler, m) {
@@ -310,7 +331,12 @@ PYBIND11_MODULE(efficient_krp_sampler, m) {
     .def(py::init<int64_t, int64_t, py::list>()) 
     .def("computeM", &CP_ALS::computeM)
     .def("KRPDrawSamples", &CP_ALS::KRPDrawSamples)
+    .def("get_G_pinv", &CP_ALS::get_G_pinv)
     ;
+
+  py::class_<CP_Decomposition>(m, "CP_Decomposition")
+    .def(py::init<int64_t, py::list>()) 
+    .def("materialize_partial_evaluation", &CP_Decomposition::materialize_partial_evaluation);
 }
 
 /*
---
+++ leave_one_test.py
@@ -0,0 +1,87 @@
+import numpy as np
+import numpy.linalg as la
+import matplotlib.pyplot as plt
+import time
+import json
+
+import cppimport.import_hook
+from cpp_ext.efficient_krp_sampler import CP_ALS, CP_Decomposition 
+
+def symmetrize(buf):
+    return np.triu(buf, 1) + np.triu(buf, 1).T + np.diag(np.diag(buf))
+
+def krp(mats):
+    if len(mats) == 1:
+        return mats[0]
+    else:
+        running_mat = np.einsum('ik,jk->ijk', mats[0], mats[1]).reshape((mats[0].shape[0] * mats[1].shape[0], mats[0].shape[1]))
+        
+        for i in range(2, len(mats)):
+            running_mat = np.einsum('ik,jk->ijk', running_mat, mats[i]).reshape((running_mat.shape[0] * mats[i].shape[0], mats[0].shape[1]))
+
+        return running_mat
+
+def chain_had_prod(matrices):
+    res = np.ones(matrices[0].shape)
+    for mat in matrices:
+        res *= mat
+    return res
+
+def execute_leave_one_tests():
+    I = 4
+    N = 4
+    J = 10000
+    R = 2
+    j = 3
+    U_lhs = [np.random.rand(I, R) for _ in range(N)]
+    U_rhs = [np.random.rand(I, R) for _ in range(N)]
+
+    cp_als = CP_ALS(J, R, U_lhs)
+    samples = np.zeros((N, J), dtype=np.uint64)
+    sampled_rows = np.zeros((J, R), dtype=np.double)
+    g_pinv = np.zeros((R, R))
+
+    print("Starting evaluation...")
+    cp_als.KRPDrawSamples(j, samples, sampled_rows)
+    cp_als.get_G_pinv(g_pinv)
+    g_pinv = symmetrize(g_pinv)
+    g = la.pinv(g_pinv)
+    leverage_score_sum = np.sum(g_pinv * g)
+
+    # True leverage score sum
+    full_lhs = krp(U_lhs[:j] + U_lhs[j+1:])
+    full_rhs = krp(U_rhs[:j] + U_rhs[j+1:]) @ U_rhs[j].T
+
+    #krp_q = la.qr(krp_materialized)[0]
+    #krp_norms = la.norm(krp_q, axis=1) ** 2
+
+    leverage_scores = np.sum((sampled_rows @ g_pinv) * sampled_rows, axis=1)
+    weights = 1.0 / np.sqrt(leverage_scores * J / leverage_score_sum)
+
+    weighted_lhs = np.einsum('i,ij->ij', weights, sampled_rows)
+
+    rhs_implicit = CP_Decomposition(R, U_rhs)
+    partial_evaluation = np.zeros((J, R))
+    rhs_implicit.materialize_partial_evaluation(samples, j, partial_evaluation)
+    unweighted_rhs = partial_evaluation @ U_rhs[j].T
+    weighted_rhs = np.einsum('i,ij->ij', weights, unweighted_rhs)
+
+    # Compute the true solution 
+    elwise_prod = chain_had_prod([U_lhs[i].T @ U_rhs[i] for i in range(N) if i != j])
+    true_soln = U_rhs[j] @ elwise_prod
+    approx_soln = weighted_rhs.T @ weighted_lhs @ g_pinv
+
+    numpy_soln, _, _, _ = la.lstsq(full_lhs, full_rhs, rcond=None)
+    numpy_soln = numpy_soln.T
+
+    print(true_soln)
+    print(approx_soln)
+    print(numpy_soln)
+
+
+if __name__=='__main__':
+    execute_leave_one_tests()
+
+
+
+
