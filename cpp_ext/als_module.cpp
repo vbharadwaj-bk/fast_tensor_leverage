7d33f2151dc7344c274304a3a5a08338082afacb
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2022-11-20 12:21:27 -0800

    Ready to test the memory-efficient version of the MTTKRP.

--- cpp_ext/als_module.cpp
+++ cpp_ext/als_module.cpp
@@ -1,6 +1,8 @@
 //cppimport
 #include <iostream>
 #include "common.h"
+#include "cblas.h"
+#include "lapacke.h"
 
 using namespace std;
 
@@ -36,28 +38,39 @@ public:
 
 class __attribute__((visibility("hidden"))) LowRankTensor : public Tensor {
 public:
+    vector<uint64_t> dims;
     unique_ptr<NPBufferList<double>> U_py_bufs;
+    uint32_t N;
     uint64_t R, J;
-    Buffer<double> partial_evaluation; 
+    uint64_t max_rhs_rows;
 
-    LowRankTensor(uint64_t R, uint64_t J, py::list U_py)
+    // This is a pointer, since it will be dynamically allocated
+    // based on the tensor mode 
+    Buffer<double>* rhs_buf; 
+    Buffer<double> partial_evaluation;
+
+    LowRankTensor(uint64_t R, uint64_t J,
+        uint64_t max_rhs_rows, 
+        py::list U_py)
     :
     U_py_bufs(new NPBufferList<double>(U_py)),
     partial_evaluation({J, R})
     {
+        this->max_rhs_rows = max_rhs_rows;
         this->J = J;
         this->R = R;
+        this->N = U_py_bufs->length;
+        for(int i = 0; i < N; i++) {
+            dims.push_back(info.shape[0]);
+        }
     }
 
     // Convenience method for RHS sampling 
     void materialize_partial_evaluation(Buffer<uint64_t> &samples, 
-        uint64_t j 
-        ) {
+        uint64_t j) {
 
         // Assume sample matrix is N x J, result is J x R
         std::fill(partial_evaluation(), partial_evaluation(J, 0), 1.0);
-
-        uint32_t N = U_py_bufs->length;
 
         #pragma omp parallel for 
         for(uint32_t i = 0; i < J; i++) {
@@ -71,13 +84,73 @@ public:
         }
     }
 
+    /*
+    * Fills rhs_buf with an evaluation of the tensor starting from the
+    * specified index in the array of samples. 
+    */
+    void materialize_rhs(Buffer<uint64_t> &samples, uint64_t j, uint64_t row_pos) {
+        Buffer<double> &temp_buf = (*rhs_buf);
+        uint64_t max_range = min(row_pos + max_rhs_rows, J);
+        uint32_t M = (uint32_t) (max_range - row_pos);
+        uint32_t N = (uint32_t) dims[j]; 
+
+        cblas_dgemm(
+            CblasRowMajor,
+            CblasNoTrans,
+            CblasTrans,
+            M,
+            N,
+            R,
+            1.0,
+            partial_evaluation(row_pos, 0),
+            R,
+            U_py_bufs->buffers[k](),
+            R,
+            0.0,
+            temp_buf(),
+            N
+        );
+    }
+
+    void preprocess(Buffer<uint64_t> &samples, uint64_t j) {
+        materialize_partial_evaluation(samples, j);
+    }
+
     void execute_downsampled_mttkrp(
             Buffer<uint64_t> &samples, 
             Buffer<double> &lhs,
             uint64_t j,
             Buffer<double> &result
             ) {
-        cout << "Hello world!" << endl;
+        
+        rhs_buf = new Buffer<double>({max_rhs_rows, dims[j]});
+        Buffer<double> &temp_buf = (*rhs_buf);
+        preprocess(samples, j);
+
+        // Result is a dims[j] x R matrix
+        std::fill(result(), result(dims[j], 0), 0.0);
+
+        for(uint64_t i = 0; i < J; i += max_rhs_rows) {
+            materialize_rhs(samples, j, i);
+
+            cblas_dgemm(
+                CblasRowMajor,
+                CblasTrans,
+                CblasNoTrans,
+                (uint32_t) dims[j],
+                (uint32_t) R,
+                (uint32_t) max_rhs_rows,
+                1.0,
+                temp_buf(),
+                (uint32_t) dims[j],
+                lhs(row_pos, 0),
+                (uint32_t) R,
+                1.0,
+                result(),
+                (uint32_t) R
+            );
+        }
+        delete rhs_buf; 
     }
 };
 
@@ -89,8 +162,8 @@ public:
 };
 
 PYBIND11_MODULE(als_module, m) {
-    py::class_<Tensor>(m, "Tensor");
-        //.def("execute_downsampled_mttkrp_py", &Tensor::execute_downsampled_mttkrp_py);
+    py::class_<Tensor>(m, "Tensor")
+        .def("execute_downsampled_mttkrp_py", &Tensor::execute_downsampled_mttkrp_py);
     py::class_<LowRankTensor, Tensor>(m, "LowRankTensor")
         .def(py::init<uint64_t, uint64_t, py::list>());
     py::class_<ALS>(m, "ALS")
