3a82575cedfee3eee6c32b642681726ad54a6097
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2022-12-09 00:10:52 -0800

    Larsen & Kolda sampler appears to be working...

--- cpp_ext/larsen_kolda_sampler.hpp
+++ cpp_ext/larsen_kolda_sampler.hpp
@@ -18,7 +18,7 @@ using namespace std;
 class __attribute__((visibility("hidden"))) LarsenKoldaSampler : public Sampler {
 public:
 
-    vector<Buffer<double>> factor_leverage;
+    vector<unique_ptr<Buffer<double>>> factor_leverage;
     Buffer<double> leverage_sums;
     vector<unique_ptr<std::discrete_distribution<uint64_t>>> distributions;
 
@@ -31,26 +31,30 @@ public:
             leverage_sums({N})
     {
         for(uint32_t k = 0; k < N; k++) {
-            uint64_t Ij = U[k].shape[0];
-            factor_leverage.emplace_back(initializer_list<uint64_t>{Ij});
+            uint64_t Ik = U[k].shape[0];
+            factor_leverage.emplace_back();
+            factor_leverage[k].reset(new Buffer<double>({Ik}));
             distributions.emplace_back();
             update_sampler(k);
         } 
     }
 
     void update_sampler(uint64_t j) {
+        uint64_t Ij = U[j].shape[0];
         Buffer<double> pinv({R, R});
         compute_pinv(U[j], pinv);
-        compute_DAGAT(U[j](), pinv(), factor_leverage[j](), U[j].shape[0], R);
-        distributions[j].reset(new discrete_distribution<uint64_t>(factor_leverage[j](), 
-                                                factor_leverage[j](U[j].shape[0])));
 
-        leverage_sums[j] = 0.0;
+        Buffer<double> &leverage = *(factor_leverage[j]);
+        compute_DAGAT(U[j](), pinv(), leverage(), Ij, R);
+
+        distributions[j].reset(new discrete_distribution<uint64_t>(leverage(), 
+            leverage(Ij)));
 
         double total = 0.0;
+
         #pragma omp parallel for reduction(+: total)
         for(uint64_t i = 0; i < U[j].shape[0]; i++) {
-            total += factor_leverage[j][i];
+            total += leverage[i];
         }
         leverage_sums[j] = total;
     }
@@ -59,13 +63,14 @@ public:
         std::fill(weights(), weights(J), 0.0-log((double) J));
         for(uint32_t k = 0; k < N; k++) {
             if(k != j) {
+                Buffer<double> &leverage = *(factor_leverage[k]);
                 Buffer<uint64_t> row_buffer({J}, samples(k, 0)); // View into a row of the samples array
                 std::discrete_distribution<uint64_t> &dist = (*(distributions[k]));
-                // Random number generation not parallelized on CPU, since it is cheap 
+
                 for(uint64_t i = 0; i < J; i++) {
                     uint64_t sample = dist(gen);
                     row_buffer[i] = sample;
-                    weights[i] += log(factor_leverage[k][sample]) - log(leverage_sums[k]);
+                    weights[i] += log(leverage_sums[k]) - log(leverage[sample]); 
                 }
             }
         }
