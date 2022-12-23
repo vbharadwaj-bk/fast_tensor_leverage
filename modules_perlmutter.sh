2c6a35b4315270a8c58f2934ad9816987b2282d1
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2022-12-23 12:07:58 -0800

    The experiment appears to be working...

--- modules_perlmutter.sh
+++ modules_perlmutter.sh
@@ -1,5 +1,10 @@
-export CC=CC
-export CXX=CC
+export CC=gcc
+export CXX=g++
+
+conda deactivate
+module load python
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/global/homes/v/vbharadw/OpenBLAS_install/lib
 
 export OMP_MAX_ACTIVE_LEVELS=2 
 export OMP_NUM_THREADS=64
