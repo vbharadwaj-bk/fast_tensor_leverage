c1c0531b1c5c312dd089427cd29bafdf96645dda
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2022-12-26 16:56:17 -0800

    Moved ALS methods to another file.

--- als_test.py
+++ als_test.py
@@ -8,70 +8,13 @@ import h5py
 import ctypes
 
 from common import *
+from tensors import *
+from als import *
 
 import cppimport.import_hook
 from cpp_ext.efficient_krp_sampler import CP_ALS 
 from cpp_ext.als_module import Tensor, LowRankTensor, SparseTensor, PyFunctionTensor, ALS 
 from cpp_ext.efficient_krp_sampler import CP_ALS
-from tensors import *
-
-def als(lhs, rhs, J, method, iter):
-    data = []
-
-    als = ALS(lhs.ten, rhs.ten)
-
-    if method != "exact":
-        als.initialize_ds_als(J, method)
-
-    residual = lhs.compute_diff_resid(rhs)
-    rhs_norm = np.sqrt(rhs.ten.get_normsq())
-
-    #try:
-    if True:
-        for i in range(iter):
-            for j in range(lhs.N):
-                # This is used just to check for NaN values.
-                g = chain_had_prod([lhs.U[i].T @ lhs.U[i] for i in range(lhs.N) if i != j])
-                detected_nan = np.any(np.isnan(g))
-
-                if detected_nan:
-                    print("Found a NaN value!")
-
-                sigma_lhs = np.zeros(lhs.R, dtype=np.double) 
-                lhs.ten.get_sigma(sigma_lhs, -1)
-
-                als.execute_exact_als_update(j, True, True)
-                residual = lhs.compute_diff_resid(rhs)
-
-                if method != "exact":
-                    als.execute_ds_als_update(j, True, True)
-                    residual_approx = lhs.compute_diff_resid(rhs)
-                else: 
-                    residual_approx = residual
-
-                if residual > 0:
-                    ratio = residual_approx / residual
-                else:
-                    ratio = 1.0
-
-                fit = lhs.compute_estimated_fit(rhs)
-                print(f"Ratio: {ratio}, Residual: {residual_approx / rhs_norm}, Fit: {fit}")
-
-                data_entry = {}
-                data_entry["fit"] = fit 
-                data_entry["exact_solve_residual"] = residual
-                data_entry["approx_solve_residual"] = residual_approx
-                data_entry["exact_solve_residual_normalized"] = residual / rhs_norm
-                data_entry["approx_solve_residual_normalized"] = residual_approx / rhs_norm
-                data_entry["rhs_norm"] = rhs_norm
-                data_entry["ratio"] = ratio
-                data_entry["j"] = j 
-                
-                data.append(data_entry)
-        return data 
-    #except:
-    #    print("Caught SVD unconverged exception, terminating and returning trace...")
-    #    return data
 
 def sparse_tensor_test():
     J = 2 ** 16 
@@ -84,8 +27,8 @@ def sparse_tensor_test():
     #R_values = [4, 8, 16, 32, 64, 128]
     R_values = [25]
 
-    #rhs = PySparseTensor("/pscratch/sd/v/vbharadw/tensors/uber.tns_converted.hdf5", lookup="sort")
-    rhs = PySparseTensor("/pscratch/sd/v/vbharadw/tensors/amazon-reviews.tns_converted.hdf5", lookup="sort")
+    rhs = PySparseTensor("/pscratch/sd/v/vbharadw/tensors/uber.tns_converted.hdf5", lookup="sort")
+    #rhs = PySparseTensor("/pscratch/sd/v/vbharadw/tensors/amazon-reviews.tns_converted.hdf5", lookup="sort")
 
     for R in R_values: 
         result[R] = {}
@@ -94,7 +37,7 @@ def sparse_tensor_test():
             for trial in range(trial_count):
                 lhs = PyLowRank(rhs.dims, R)
                 lhs.ten.renormalize_columns(-1)
-                result[R][sampler].append(als(lhs, rhs, J, sampler, iterations))
+                result[R][sampler].append(als_exact_comparison(lhs, rhs, J, sampler, iterations))
 
     with open('outputs/lk_uber_comparison.json', 'w') as outfile:
         json.dump(result, outfile, indent=4)
