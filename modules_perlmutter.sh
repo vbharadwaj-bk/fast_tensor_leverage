53fc5c8490d0a058161c560458331cdfa02f2e43
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2022-12-27 12:31:22 -0800

    Established that the core binding / affinity flags degrade performance.

--- modules_perlmutter.sh
+++ modules_perlmutter.sh
@@ -4,6 +4,10 @@ export CXX=g++
 conda deactivate
 module load python
 
+# Both of these flags appear to degrade performance...
+#export OMP_PLACES=cores
+#export OMP_PROC_BIND=close
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/global/homes/v/vbharadw/OpenBLAS_install/lib
 
 export OMP_MAX_ACTIVE_LEVELS=2 
