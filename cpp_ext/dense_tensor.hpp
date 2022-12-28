6855343724ba786f7212bb9f14069279864eed64
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2022-12-27 20:16:53 -0800

    Ready to test dense tensor class.

--- cpp_ext/dense_tensor.hpp
+++ cpp_ext/dense_tensor.hpp
@@ -13,10 +13,18 @@ template<typename VAL_T>
 class __attribute__((visibility("hidden"))) DenseTensor : public BlackBoxTensor {
 public:
     Buffer<VAL_T> tensor;
+    Buffer<uint64_t> prefix_prod;
     DenseTensor(py::array_t<VAL_T> tensor_py, uint64_t max_rhs_rows) 
-    : tensor(tensor_py) 
+    :   tensor(tensor_py),
+        dims(tensor.shape)
+        prefix_prod({dims.size()})
     {
         this->max_rhs_rows = max_rhs_rows;
+
+        prefix_prod[dims.size() - 1] = 1;
+        for(int i = dims.size() - 2; i >= 0; i--) {
+            prefix_prod[i] = prefix_prod[i+1] * dims[i+1];
+        }
     }
 
     void preprocess(Buffer<uint64_t> &samples, uint64_t j) {
@@ -24,12 +32,27 @@ public:
         // before repeatedly calling ``materialize_rhs" 
     }
 
-    void materialize_rhs(Buffer<uint64_t> &samples_transpose, uint64_t j, uint64_t row_pos) {
-        Buffer<double> &temp_buf = (*rhs_buf);
-        uint64_t max_range = min(row_pos + max_rhs_rows, samples_transpose.shape[0]);
-        uint32_t M = (uint32_t) (max_range - row_pos);
-        uint32_t Ij = (uint32_t) tensor.shape[j]; 
-        uint32_t tensor_dim = tensor.shape.size(); 
+    void materialize_rhs(Buffer<uint64_t> &samples_transpose, 
+            uint64_t j, 
+            Buffer<double> &rhs_buf) {
 
+        uint64_t num_samples = samples_transpose.shape[0];
+        uint64_t Ij = dims[j];
+        uint64_t N = dims.size();
+        uint64_t ldt = prefix_prod[j];
+
+        #pragma omp parallel for 
+        for(uint64_t i = 0; i < num_samples; i++) {
+            uint64_t offset = 0;
+            for(int u = 0; u < N; u++) {
+                if(u != j) {
+                    offset += samples_transpose[i * N + u] * prefix_prod[u];
+                }
+            }
+
+            for(uint64_t k = 0; k < Ij; k++) {
+                rhs_buf[i * Ij + k] = tensor[offset + k * ldt];                
+            }
+        }
     };
 };
