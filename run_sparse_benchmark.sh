458c6484c955f6b99aba90e2533855d105d372c5
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2023-01-18 00:01:54 -0800

    Updated plotting backup.

--- run_sparse_benchmark.sh
+++ run_sparse_benchmark.sh
@@ -2,8 +2,9 @@
 #SBATCH -N 4
 #SBATCH -C cpu
 #SBATCH -q regular
-#SBATCH -t 10:00:00
+#SBATCH -t 12:00:00
 #SBATCH -A m1266 
+#SBATCH -q regular 
 
 . modules_perlmutter.sh
 srun -N 4 -n 4 -u python sparse_tensor_benchmark.py 
