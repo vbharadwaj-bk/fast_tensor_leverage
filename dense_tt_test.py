dc2e5e007699bccb447fed0c8ba399552131a701
rakhshab <rakhshab@mila.quebec>
2023-11-29 12:05:14 -0500

    added rtt-svd

--- dense_tt_test.py
+++ dense_tt_test.py
@@ -8,13 +8,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score
-import tensorly as tl
+
 
 import logging, sys
 import time
-
-import cppimport
-import cppimport.import_hook
 
 from tensors.tensor_train import *
 from algorithms.tt_als import *
@@ -25,19 +22,30 @@ N = 4
 rho = [0.5,0.1]
 
 def test_all_tt_dense_tensor(dataset, R, J):
-    tests = ["tt-svd","rtt-als","rsvd"]
+    tests = ["tt-svd","rtt-svd","rtt-als","rsvd"]
     print("Loaded dataset...")
     ground_truth, labels = get_coil_tensor(dataset)
+    print(labels.shape)
     # x_train, x_test, y_train, y_test = train_test_split(ground_truth.data, labels, train_size=0.5, random_state=42)
 
     for test in tests:
-        outfile = f'outputs/dense_tt/{test}_J_{j}.json'
+        outfile = f'outputs/dense_tt/{test}.json'
         start = time.time()
+        u = None
+
         if test == "tt-svd":
-            print("Starting SVD...")
-            tt_svd = TensorTrainSVD(ground_truth)
-            cores = tt_svd.tt_regular_svd(R)
+            print("Starting TT-SVD...")
+            tt_svd = TensorTrainSVD(ground_truth,R)
+            cores = tt_svd.tt_regular_svd()
+            u = cores[-1]
+
+        elif test == "rtt-svd":
+            print("Starting rTT-SVD...")
+            cores = tt_svd.tt_randomized_svd()
+            u = cores[-1]
+
         elif test == "rtt-als":
+            print("Starting rTT-ALS...")
             tt_approx = TensorTrain(ground_truth.shape, [R] * (ground_truth.N - 1))
             tt_approx.place_into_canonical_form(0)
 
@@ -46,49 +54,45 @@ def test_all_tt_dense_tensor(dataset, R, J):
             # print(tt_als.compute_exact_fit())
             tt_approx.build_fast_sampler(0, J=j)
             tt_als.execute_randomized_als_sweeps(num_sweeps=1, J=j)
-            # last_core = np.reshape(cores[-1], (7200, -1))
+            u = tt_als.tt_approx.U[0]
+            print(u.shape)
+            u = u.transpose((2,1,0))
+            print(u.shape)
+
 
 
         elif test == "rsvd":
             ground_truth_reshaped = ground_truth.data.reshape((7200,32*32*3))
             print("Starting rSVD...")
             u,s,v = randomized_svd(ground_truth_reshaped, R)
-
-
+            print(u.shape)
             # last_core = np.reshape(np.transpose(cores[-1], (1, 0, 2)), (7200, -1))
 
+        if u is not None and test != "rsvd":
+            new_dim1, new_dim2 = u.shape[1], u.shape[0]*u.shape[2]
+            last_core = u.reshape((new_dim1, new_dim2))
+            print(last_core.shape)
+        elif u is not None and test == "rsvd":
+            last_core = u
         else:
             continue
+
         end = time.time()
-        core_dims = cores[-1].shape
-        new_dim1 = core_dims[1]
-        new_dim2 = core_dims[0] * core_dims[2]
-        last_core = np.reshape(np.transpose(cores[-1], (1, 0, 2)), (new_dim1, new_dim2))
-        if test == "rsvd":
-            last_core = u
-        if last_core.shape[0] != labels.shape[0]:
-            raise ValueError(
-                f"Shape mismatch: last_core has {last_core.shape[0]} samples, labels has {labels.shape[0]} samples")
+
         knn = KNeighborsClassifier(n_neighbors=1)
         cv_scores = cross_val_score(knn, last_core, labels, cv=10, scoring='accuracy')
         accuracy = np.mean(cv_scores)
         total_time = end - start
+
         result = {
             "J": J,
             "test_name": test,
             "time": total_time,
             "acc": accuracy,
-            "loss": 1-accuracy
         }
         with open(outfile, 'w') as f:
             json.dump(result, f)
 
-
 if __name__ == '__main__':
     j = 2000
-    test_all_tt_dense_tensor("coil-100", R = 5, J = j)
-
-
-
-
-# core_transpose = np.transpose(cores[-1], axes=[1, 0, 2]).reshape(7200,5)
+    test_all_tt_dense_tensor("coil-100", 5, j)
