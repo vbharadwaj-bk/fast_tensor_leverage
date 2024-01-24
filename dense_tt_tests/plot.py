9c6c6a7c39b5ab94494f1d96f936c232490ffc39
rakhshab <rakhshab@mila.quebec>
2024-01-24 18:16:05 -0500

    changes updated

--- dense_tt_tests/plot.py
+++ dense_tt_tests/plot.py
@@ -3,15 +3,15 @@ import json
 import os
 import matplotlib.pyplot as plt
 
-r_true = 5
-std_noise = 10
+r_true = 10
+std_noise = 0.1
 path = "./outputs/dense_tt/synthetic/"
 methods = {"tt-svd": {"time": [], "fit": []},
            "randomized_tt-svd": {"time": [], "fit": []},
            "tt-als": {"time": [], "fit": []},
            "randomized_tt-als": {"time": [], "fit": []},
            }
-for i in range(1, 3):
+for i in range(1, 6):
     for method in methods.keys():
         file_path = f'{path}{std_noise}_{method}_data{i}_rank{r_true}.json'
         with open(file_path, 'r') as file:
@@ -19,9 +19,7 @@ for i in range(1, 3):
             methods[method]['fit'].append(file['fit'])
             methods[method]['time'].append(file['time'])
 
-# tensor_sizes = [100, 200, 300, 400, 500]
-# colors = ['red', 'blue', 'green', 'orange']
-tensor_sizes = [100, 200]
+tensor_sizes = [100, 200, 300, 400, 500]
 colors = ['red', 'blue', 'green', 'orange']
 
 fig, ax = plt.subplots()
@@ -30,17 +28,18 @@ for (method, color) in zip(methods.keys(), colors):
 
     ax.plot(tensor_sizes, methods[method]['fit'], color=color, label=method)
 
+
 ax.set_xlabel('Tensor Size')
 ax.set_ylabel('fit')
 ax.set_title('synthetic')
 ax.grid(True)
 ax.set_xticks(tensor_sizes)
 
-# ax.set_ylim(0, 1.1)
+
 ax.legend()
 
 plt.show()
-outfile = f'outputs/dense_tt/synthetic/plots/{std_noise}_fit_rank{r_true}.png'
+outfile = f'outputs/dense_tt/synthetic/plots/noise_free_fit_rank{r_true}.png'
 plt.savefig(outfile)
 
 
